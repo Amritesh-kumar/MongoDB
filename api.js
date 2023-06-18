f4b772d92b63ccbd920f4a8a8b60b3f07ae19257
Amritesh <63038816+Amritesh-kumar@users.noreply.github.com>
2023-06-18 22:27:34 +0530

    Update api.js
    Added Insert code

--- api.js
+++ api.js
@@ -3,15 +3,27 @@ const dbConnect = require('./mongodb');
 const mongodb = ('mongodb');
 const app = express();
 
+// API test
 app.get('/api',(req, res) => {
     res.json("Hello");
 });
 
+// Get record 
 app.get('/getdata', async(req, res)=>{
     let data = await dbConnect();
     data = await data.find().toArray();
     console.log(data);
     res.send(data);
+})
+
+// Insert code
+app.use(express.json());
+
+app.post('/insert',async (req, res)=>{
+    //console.log(req.body);
+    let data = await dbConnect();
+    let result = data.insertOne(req.body);
+    res.send(result);
 })
 
 app.listen(3000);
