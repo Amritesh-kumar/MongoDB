6ce5a9180ccc65b615df08d5e359b57af418389d
Amritesh <63038816+Amritesh-kumar@users.noreply.github.com>
2023-06-18 22:15:52 +0530

    Update api.js

--- api.js
+++ api.js
@@ -7,4 +7,11 @@ app.get('/api',(req, res) => {
     res.json("Hello");
 });
 
+app.get('/getdata', async(req, res)=>{
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data);
+    res.send(data);
+})
+
 app.listen(3000);
